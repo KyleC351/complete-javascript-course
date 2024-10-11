d023dced6f060b9bb2d8fa5e27adc8d734d8832c
KyleC351 <christensen.kyle.zan@gmail.com>
2024-10-11 12:42:47 -0600

    Update script.js

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -5,6 +5,18 @@ const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 
+//Practice Strings
+const getCode = str => str.slice(0, 3).toUpperCase;
+
+for(const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll('_', ' ')} ${getCode(from)} ${getCode(to)} (${time.replace(':', 'h')})`.padStart(36);
+  console.log(output);
+}
+
+
+
+
 const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 const openingHours = {
     [weekdays[3]]: {
@@ -31,7 +43,7 @@ const restaurant = {
 
   // ES6 enhanced object literals
   openingHours,
-
+  
   
   
 
@@ -53,6 +65,296 @@ const restaurant = {
   },
 
 };
+
+// Split and join
+console.log('a+very+nice+string'.split('+'));
+console.log('Jonas Schmedtmann'.split(' '));
+
+const [firstName, lastName] = 'Jonas Schmedtmann'.split(' ');
+
+const newName =['mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizeName = function(name){
+  const names = name.split(' ');
+  const namesUpper = [];
+
+  for(const n of names) {
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  console.log(namesUpper.join(' '));
+}
+
+capitalizeName('jessica ann smith davis');
+capitalizeName('kyle christensen');
+
+//Padding a string
+const message = 'Go to gate 23!'
+console.log(message.padStart(20, '+').padEnd(30, '+'));
+console.log('Jonas'.padStart(20, '+').padEnd(30, '+'));
+
+const maskCreditCard = function(number) {
+  const str = number + '';
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+}
+
+console.log(maskCreditCard(4337846787656808));
+console.log(maskCreditCard('2988659034798579'));
+
+//Repeat
+const message2 = 'Bad weather... All Departures Delayed...';
+console.log(message2.repeat(5));
+
+const planesInLine = function(n){
+  console.log(`There are ${n} planes in line ${'p'.repeat(n)}`);
+}
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
+
+
+
+// const airline = 'TAP Air Portugal';
+
+// console.log(airline.toLowerCase());
+// console.log(airline.toUpperCase());
+
+// // Fix capitalization in name
+// const passenger = 'jOnAS'; // Jonas
+// const passengerLower = passenger.toLowerCase();
+// const passengerCorrect = passengerLower[0].toUpperCase() + passengerLower.slice(1);
+// console.log(passengerCorrect);
+
+// // Comparing emails
+// const email = 'hello@jonas.io';
+// const loginEmail = '  Hello@Jonas.Io \n';
+
+// // const lowerEmail = loginEmail.toLowerCase();
+// // const trimmedEmail = lowerEmail.trim();
+
+// const normalizedEmail = loginEmail.toLowerCase().trim();
+// console.log(normalizedEmail);
+// console.log(email === normalizedEmail);
+
+// // replacing
+// const priceGB = '288,97#';
+// const priceUS = priceGB.replace('#', '$').replace(',', '.');
+// console.log(priceUS);
+
+// const announcement  = 'All passengers come to boarding door 23. Boarding door 23!';
+
+// console.log(announcement.replace('door', 'gate'));
+// // console.log(announcement.replaceAll('door', 'gate'));
+// console.log(announcement.replace(/door/g, 'gate'));
+
+// // Booleans
+// const plane = 'Airbus A320neo';
+// console.log(plane.includes('A320'));
+// console.log(plane.includes('Boeing'));
+// console.log(plane.startsWith('Airb'));
+
+// if(plane.startsWith('Airbus') && plane.endsWith('neo')){
+//   console.log('Part of the NEW Airbus family');
+// }
+
+// //Practice exercise
+// const checkBaggage = function(items){
+//  const baggage = items.toLowerCase();
+//   if(baggage.includes('knife') || baggage.includes('gun')){
+//     console.log('You are NOt allowed on board');
+//   }else{
+//     console.log('Welcome aboard!');
+//   }
+
+// }
+// checkBaggage('I have a laptop, some Food and a pocket Knife');
+// checkBaggage('Socks and camera');
+// checkBaggage('Got some snacks and a gun for protection');
+
+
+// const airline = 'TAP Air Portugal';
+// const plane = 'A320';
+
+// console.log(plane[0]);
+// console.log(plane[1]);
+// console.log(plane[2]);
+// console.log('B737'[0]);
+
+// console.log(airline.length);
+// console.log('B737'.length);
+
+// console.log(airline.indexOf('r'));
+// console.log(airline.lastIndexOf('r'));
+// console.log(airline.indexOf('Portugal'));
+
+// console.log(airline.slice(4));
+// console.log(airline.slice(4, 7));
+
+// console.log(airline.slice(0, airline.indexOf(' ')));
+// console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+// console.log(airline.slice(-2));
+// console.log(airline.slice(1, -1));
+
+// const checkMiddleSeat = function(seat) {
+//   // B and E are middle seats
+//   const s = seat.slice(-1);
+//   if (s === 'B' || s === 'E')
+//     console.log('You got the middle seat 🙁');
+//     else console.log('You got lucky 🫡');
+// };
+
+// checkMiddleSeat('11B');
+// checkMiddleSeat('23C');
+// checkMiddleSeat('3E');
+
+// console.log(new String ('Jonas'));
+// console.log(typeof new String ('Jonas'));
+
+
+// console.log(typeof new String ('Jonas').slice(1));
+
+
+// const question = new Map([
+//   ['question', 'What is the best programing language in the world?'],
+//   [1, 'c'],
+//   [2, 'java'],
+//   [3, 'JavaScript'],
+//   ['correct', 3],
+//   [true, 'Correct!'],
+//   [false, 'Try again!'],
+// ]);
+// console.log(question);
+
+// //Convert object to map
+// console.log(Object.entries(openingHours));
+// const hoursMap = new Map(Object.entries(openingHours));
+// console.log(hoursMap);
+
+
+// //Quiz app
+// console.log(question.get('question'));
+// for (const [key, value] of question) {
+//   if(typeof key === 'number') console.log('Answer ${key}: ${value}');
+// }
+// // const answer = Number(prompt('Your answer'));
+// const answer = 3;
+// console.log(answer);
+
+// console.log(question.get(question.get('correct') === answer));
+
+// // Covert map to array
+// console.log([...question]);
+// // console.log(question.entries());
+// console.log(...question.keys());
+// console.log(...question.values());
+
+// const rest = new Map();
+// rest.set('name', 'Classico Italiano');
+// rest.set(1, 'Fireze, Italy');
+// console.log(rest.set(2, 'Lisbon, Portugal'));
+
+// rest.set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+// .set('open', 11)
+// .set('close', 23)
+// .set(true, 'We are open :D')
+// .set(false, 'We are closed :(');
+
+// console.log(rest.get('name'));
+// console.log(rest.get(true));
+// console.log(rest.get(1));
+
+// const time = 21;
+// console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+// console.log(rest.has('categories'));
+// rest.delete(2);
+// // rest.clear();
+
+// const arr = [1, 2];
+// rest.set(arr, 'Test');
+// rest.set(document.querySelector('h1'), 'Heading');
+// console.log(rest);
+// console.log(rest.size);
+
+// console.log(rest.get(arr));
+
+// const ordersSet = new Set(['Pasta', 'Pizza', 'Pizza', 'Risotto', 'Pasta', 'Pizza']);
+// console.log(ordersSet);
+
+// console.log(new Set('Jonas'));
+
+// console.log(ordersSet.size);
+// console.log(ordersSet.has('Pizza'));
+// console.log(ordersSet.has('Bread'));
+// ordersSet.add("Garlic Bread");
+// ordersSet.add('Garlic Bread');
+// ordersSet.delete('Risotto');
+// // ordersSet.clear();
+// console.log(ordersSet);
+
+// for(const order of ordersSet) console.log(order);
+
+// //Example
+// const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+// const staffUnique = [...new Set(staff)];
+// console.log(staffUnique);
+// console.log(new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size );
+// console.log(new Set('jonasschmedtmann').size);
+
+// const properties = Object.keys(openingHours);
+// console.log(properties);
+
+// let openStr = 'We are open on ${properties.length} days : '
+
+// for (const day of Object.keys(openingHours)) {
+//   console.log(day);
+// }
+
+// console.log(openStr);
+
+// // Property Values
+// const values = Object.values(openingHours);
+// console.log(values);
+
+// //Entire object
+// const entries = Object.entries(openingHours);
+// console.log(entries);
+
+// for(const [day, {open, close}] of entries) {
+//   console.log('On ${day} we open at ${open} and close at ${close}');
+// }
+
+// if (restaurant.openingHours && restaurant.openingHours.mon)
+//    console.log(restaurant.openingHours.mon.open);
+
+// //  WITH optional chaining
+// console.log(restaurant.openingHours.mon?.open);
+// console.log(restaurant.openingHours?.mon?.open);
+
+// // Example
+// const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+// for (const day of days) {
+//   console.log(day);
+//   const open = restaurant.openingHours[day]?.open && 'closed';
+//   console.log('On ${day} , we open at ${open}');
+// }
+
+
+// // Methods
+// console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+// console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+
+
+// //Arrays
+
+// const users = [
+//   {name: 'Jonas', email: 'hello@jonas.io' }];
+
+//   console.log(users[0]?.name ?? 'User array empty');
+
 
 // const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
@@ -373,3 +675,115 @@ printGoals(...game.scored);
 //7.
 team1 < team2 && console.log('Team 1 is more likely to win');
 team2 < team1 && console.log('Team 2 is more likely to win');
+
+/*Let's continue with our football betting app! Keep using the 'game' variable from before.
+Your tasks: 1. Loop over the game.scored array and print each player name to the console,
+along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already
+studied how to calculate averages, you can go check if you don't remember) 3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
+Odd of victory Bayern Munich: 1.33
+Odd of draw: 3.25 Odd of victory Borrussia Dortmund: 6.5 Get the team names directly from the game object, don't hardcode them (except for "draw"). Hint: Note how the odds and the game objects have the same property names 😉
+4. Bonus: Create an object called 'scorers' which contains the names of the
+players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+{
+}
+Gnarby: 1, Hummels: 1,
+Lewandowski: 2
+GOOD LUCK 😀
+*/
+
+
+//1.
+for(const[i, player] of game.scored.entries())
+  console.log('Goal ${i + 1}: ${player}');
+
+
+//2. 
+const odds = Object.values(game.odds);
+let average = 0;
+for(const odd of odds)
+  average += odd;
+average /= odds.length;
+console.log(average);
+
+//3.
+for(const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === 'x' ? 'draw' : 'victory ${game[team]}';
+  console.log('Odd of ${teamStr} ${odd}');
+}
+
+/*Let's continue with our football betting app! This time, we have a map called 'gameEvents' (see below) with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
+Your tasks: 
+1. Create an array 'events' of the different game events that happened (no
+duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64
+was unfair. So remove this event from the game events log.
+3. Compute and log the following string to the console: "An event happened, on
+average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over 'gameEvents' and log each element to the console, marking
+whether it's in the first half or second half (after 45 min) of the game, like this: [FIRST HALF] 17: ⚽ GOAL
+*/
+
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '🔁 Substitution'],
+   [47, '⚽ GOAL'],
+  [61, '🔁 Substitution'],
+   [64, '🔶 Yellow card'],
+    [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+   [72, '🔁 Substitution'],
+    [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+   [92, '🔶 Yellow card'],
+  ]);
+
+//1.
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+//2.
+gameEvents.delete(64);
+
+//3.
+const time = [gameEvents.keys()].pop();
+console.log(time);
+console.log('An event happened, on average, every  ${time / gameEvents.size} minutes');
+
+//4.
+for(const [min, event] of gameEvents) {
+  const half = min <= 45 ? 'FIRST' : 'SECOND';
+  console.log('[${half} HALF] ${min}: ${event}');
+}
+/*Coding Challenge #4
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+The input will come from a textarea inserted into the DOM (see code below to insert the elements), and conversion will happen when the button is pressed.
+Test data (pasted to textarea, including spaces): underscore_case
+first_name Some_Variable
+calculate_AGE
+delayed_departure
+Should produce this output (5 separate console.log outputs): underscoreCase ✅
+firstName ✅✅ someVariable ✅✅✅ calculateAge ✅✅✅✅
+delayedDeparture ✅✅✅✅✅
+Hints: § Remember which character defines a new line in the textarea 😉 § The solution only needs to work for a variable made out of 2 words, like a_b § Start without worrying about the ✅. Tackle that only after you have the variable
+name conversion working 😉
+§ This challenge is difficult on purpose, so start watching the solution in case
+you're stuck. Then pause and continue!
+Afterwards, test with your own test data! */
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function(){
+  const text = document.querySelector(`textarea`).value;
+  const rows = text.split ('\n');
+  console.log(rows);
+
+  for(const [i, row] of rows.entries()) {
+    const [first, second] =  row.toLowerCase().trim().split('_');
+
+    const output = `${first}${second.replace(second[0], second[0].toUpperCase())}`;
+    console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
+  }
+
+});
